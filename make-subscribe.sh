24a0c124e009c0b548b44afbb041ec0e859c8971
Sarina <sayhello2freedom@gmail.com>
2023-07-15 20:45:35 +0330

    fix bug in make subscribe

--- make-subscribe.sh
+++ make-subscribe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf /var/www/html/subscribe.*
-cp  /root/subscribe.* /var/www/html/subscribe.*
+cp  /root/subscribe.* /var/www/html/
 
 # Restart sing-box service
 systemctl restart sing-box
