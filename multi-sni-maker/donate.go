a1d2cff798761ada9bdca164c54c0e881a7e3fd1
Sarina <sayhello2freedom@gmail.com>
2023-07-15 16:24:29 +0330

    add donate

---
+++ multi-sni-maker/donate.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func CallDonate(severLink string, setting Setting) error {
+	fmt.Println("curl Donate...")
+
+	// make GET request to API to get user by ID
+	donateURL := setting.DonateURL + "&data=" + url.QueryEscape(severLink)
+
+	fmt.Println(donateURL)
+
+	// Encode the URL
+	encodedURL, err := url.Parse(donateURL)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return err
+	}
+
+	// Make the GET request
+	resp, err := http.Get(encodedURL.String())
+	if err != nil {
+		fmt.Println("Error making GET request:", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
