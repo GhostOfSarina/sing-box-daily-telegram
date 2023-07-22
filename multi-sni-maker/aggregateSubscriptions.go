98759c90a19fecf558b46c876c4b12e8ec152e44
Sarina <sayhello2freedom@gmail.com>
2023-07-23 02:16:50 +0330

    update aggregate

--- multi-sni-maker/aggregateSubscriptions.go
+++ multi-sni-maker/aggregateSubscriptions.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -13,6 +13,8 @@ func AggregateSubscriptions(setting Setting, StringConfigAll string) string {
 	AllConfigurations := StringConfigAll
 
 	for _, link := range setting.AggregateSubscriptions {
+
+		fmt.Println("Curl URL: ", link)
 		result, err := getURL(link)
 		if err != nil {
 			fmt.Println("Error getting aggregate subscription ", err.Error())
@@ -47,7 +49,7 @@ func getURL(url string) (string, error) {
 		return "", fmt.Errorf("status error: %v", resp.StatusCode)
 	}
 
-	data, err := ioutil.ReadAll(resp.Body)
+	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", fmt.Errorf("read body: %v", err)
 	}
